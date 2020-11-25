40add6a14e7086d294b6bbdfa9a063c92eb8e417
P V Vighnesh <pvighnesh.vishnu@gmail.com>
2020-11-25 11:24:34 +0000

    Updated from Question-prompt branch Will use only the master branch from now on

--- index.js
+++ index.js
@@ -5,11 +5,52 @@
 
 //Built-in JS module for collecting data from the Users
 var readlineSync = require('readline-sync');
+var ansCheck;
+var userScore = 0;
+var flag = 0;
 
 //Welcome message
 console.log("Hello, Hope you are doing well!");
 
 //Taking user's name
-let userName = readlineSync.question("What's your name?");
-console.log("Hello " + userName + ", Good to have to here.");
+var userName = readlineSync.question("What's your name? ");
+welcomeMessage = 'Heyy ' + userName + '!!, Good to have you here!';
+console.log(welcomeMessage);
+
+
+/**
+ * Using keyInYN() function.
+ * Asking simple questions.
+ * Checking if the answer given by the user is correct or not.
+ * Displaying a message based on Users answer.
+ */
+
+  if (readlineSync.keyInYN('Care to play a Game?')) {
+
+    console.log("Let's start then!");
+    var userAnswer = readlineSync.question('Where did I study my Engineering?');
+
+  if(userAnswer === 'EPCET' || userAnswer === 'East Point' || userAnswer === 'East Point College of Engineering and Technology') {
+    flag = 1;
+    ansCheck = 'YOU ARE CORRECT! :D';
+  }
+  else {
+    flag = 0;
+    ansCheck = 'I THOUGHT WE WERE BEST FRIENDS :/';
+  }
+  }
+  else {
+    console.log("Looks like you're too busy for me :(");
+  }
+
+/**
+ * To increment or decrement user score
+ */
+if (flag == 1) {
+  userScore++;
+} else {
+  userScore--;
+}
+
+console.log("You score is: " + userScore);
 
